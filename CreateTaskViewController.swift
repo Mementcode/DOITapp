30975e2e3e23537397aaa02a900f80d9cc0614bb
callum brennan <callum.brennan@hotmail.co.uk>
2017-04-09 16:14:42 +0100

    working

---
+++ CreateTaskViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by callum brennan on 09/04/2017.
+//  Copyright © 2017 callum brennan Mementcode. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+
+}
